146e0cdefb0c7b6d856e03da90b9ac1c5607a0f5
André Vilson <andrevilson.pinto@gmail.com>
2022-12-26 12:25:12 -0300

    Implementado Repository e Interface

---
+++ src/DevIO.Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,24 @@
+﻿using AppMvcSimples.Models;
+using DevIO.Business.Interfaces;
+using DevIO.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Repository
+{
+    public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+    {
+        public EnderecoRepository(MeuDbContext context) : base(context) { }
+
+
+        public async Task<Endereco> ObterEnderecoPoFornecedor(Guid fornecedorId)
+        {
+            return await Db.Enderecos.AsNoTracking()
+                .FirstOrDefaultAsync(f => f.FornecedorId == fornecedorId);
+        }
+    }
+}
